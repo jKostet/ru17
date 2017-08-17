ae3302f6f2aa52804e472fbaa72d1a049e40d6fb
jKostet <juho.kostet@helsinki.fi>
2017-08-17 17:56:30 +0300

    Add Monolith drop gate, rename cube and ramp systems

---
+++ M_sensor_cube/M_sensor_cube.ino
@@ -0,0 +1,42 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(12, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
+
+
+  // Wait for system to power up stable
+  delay(3000); 
+}
+
+void testSensor() {
+  // switches
+  int rightSwitch = digitalRead(13);
+  int leftSwitch = digitalRead(12);
+
+  // debug print
+  Serial.print("RIGHT: ");
+  Serial.print(rightSwitch);
+  Serial.print(", LEFT: ");
+  Serial.println(leftSwitch);
+
+  // if both switches are on, send signal to cube dispenser
+  if ((rightSwitch + leftSwitch) == 0) {
+    Serial.println(", DROP CUBE");
+    dropCube();
+  } else {
+    Serial.println(", HOLD");
+  }
+  delay(200);
+}
+
+void dropCube() {
+  // some timer so cubes aren't dispensed all at once,
+  // as standing on the sensor will keep triggering it
+  // should probably dispense one every 15s? 
+}
+
+
+
+}
+
