cb6cdc78e70a3334953c5369ea57b61f33418084
jKostet <juho.kostet@helsinki.fi>
2017-08-17 15:50:27 +0300

    Test if both switches are on

--- pressure_sensor/pressure_sensor.ino
+++ pressure_sensor/pressure_sensor.ino
@@ -1,9 +1,29 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(12, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
 
+
+  // Wait for system to power up stable
+  delay(3000); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
+  
+  int rightSwitch = digitalRead(13);
+  int leftSwitch = digitalRead(12);
+  Serial.print("RIGHT: ");
+  Serial.print(rightSwitch);
+  Serial.print(", LEFT: ");
+  Serial.println(leftSwitch);
+  //Serial.println(buttonStatus);
+  if ((rightSwitch + leftSwitch) == 0) {
+    Serial.println(", DROP CUBE");
+  } else {
+    Serial.println(", HOLD");
+  }
+  delay(200);
 }
