3d41997822c1c294e193c2e4a33c3ca262e77083
jKostet <juho.kostet@helsinki.fi>
2017-08-27 12:23:24 +0300

    new dropper codex

--- M_drop_gate/M_drop_gate.ino
+++ M_drop_gate/M_drop_gate.ino
@@ -1,4 +1,7 @@
 #include <Stepper.h>
+#include <Servo.h>
+
+Servo servo1;
 
 //---( Number of steps per revolution of INTERNAL motor in 4-step mode )---
 #define STEPS_PER_MOTOR_REVOLUTION 32   
@@ -7,29 +10,32 @@
 #define STEPS_PER_OUTPUT_REVOLUTION 32 * 64  //2048  
 
 // Define the stepper with the correct pins, note that the two middle pins need to be in reverse order
-Stepper gateStepper(STEPS_PER_MOTOR_REVOLUTION, 10, 12, 11, 13);
+//Stepper gateStepper(STEPS_PER_MOTOR_REVOLUTION, 10, 12, 11, 13);
 Stepper leftStepper(STEPS_PER_MOTOR_REVOLUTION, 6, 8, 7, 9);
 Stepper rightStepper(STEPS_PER_MOTOR_REVOLUTION, 2, 4, 3, 5);
 
 int Steps2Take, i;
-
+int pos;
 
 void setup() {
   // Wait for system to power up stable
   delay(3000); 
+  servo1.attach(14);
+//  servo2.attach(15);
+  pos = 15;
   
   //Serial.begin(9600);
   
   // Trapdoor / cube sensor only has one contact.
   // Should it have two? 
   //pinMode(12, INPUT_PULLUP);
-  //pinMode(13, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
 }
 
 void loop() {
-  //testSensor();
+  testSensor();
   //openGate();
-  openTrap();
+  //openTrap();
 }
 
 
@@ -37,39 +43,40 @@ void testSensor() {
   // switches  delay(100);
 
   int rightSwitch = digitalRead(13);
-  int leftSwitch = digitalRead(12);
+  //int leftSwitch = digitalRead(12);
 
   // debug print
-  Serial.print("RIGHT: ");
-  Serial.print(rightSwitch);
-  Serial.print(", LEFT: ");
-  Serial.println(leftSwitch);
+  //Serial.print("RIGHT: ");
+  //Serial.print(rightSwitch);
+  //Serial.print(", LEFT: ");
+  //Serial.println(leftSwitch);
 
   // if both switches are on, send signal to cube dispenser
-  if ((rightSwitch + leftSwitch) == 0) {
-    openGate();
+  if ((rightSwitch) == 0) {
+    //openGate();
+    delay(5000);
+    //openGate();
+    openTrap();
   } else {
     Serial.println(", HOLD");
   }
   delay(200);
 }
 
+
 void openGate() {
+  
 
-  // OPEN GATE
-  Steps2Take  =  STEPS_PER_OUTPUT_REVOLUTION / 4;  // Rotate CW 1/4 turn
-  gateStepper.setSpeed(300);   
-  gateStepper.step((Steps2Take));
-  delay(1000);
-
-  // HOLD OPEN FOR 15s
-  // delay(15000);
-  openTrap();
-
-  Steps2Take  =  (-1)*STEPS_PER_OUTPUT_REVOLUTION / 4;  // Rotate CCW 1/4 turn  
-  gateStepper.setSpeed(300);
-  gateStepper.step(Steps2Take);
-  delay(1000);
+  for (pos = 15; pos <= 80; pos += 1) {
+    servo1.write(pos);
+    //servo2.write(80-pos);
+    delay(30);
+  }
+  for (pos = 80; pos >= 15; pos -= 1) {
+    servo1.write(pos);
+    //servo2.write(180-pos);
+    delay(30);
+  }
 
 }
 
@@ -84,7 +91,7 @@ void openTrap() {
   for (i = 0; i <= 2*Steps2Take; i += 1) {
     switch(i%2) {
       case 0:
-        rightStepper.step(-1);
+        rightStepper.step(1);
         break;
       case 1:
         leftStepper.step(-1);
@@ -94,12 +101,12 @@ void openTrap() {
   }
 
   // Keep open for five seconds
-  delay(5000);
+  delay(10000);
 
   for (i = 0; i <= 2*Steps2Take; i += 1) {
     switch(i%2) {
       case 0:
-        rightStepper.step(1);
+        rightStepper.step(-1);
         break;
       case 1:
         leftStepper.step(1);
